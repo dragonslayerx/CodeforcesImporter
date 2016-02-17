b96655d009ab8581bccb1812486529a2f73a4ae8
dragonslayer95 <dragonslayerx@outlook.com>
2016-02-17 12:31:16 +0530

    added: Problem entity

---
+++ codeforces_importer/Entity/Problem.py
@@ -0,0 +1,24 @@
+class Problem:
+    def __init__(self):
+        self.contestId = None
+        self.index = None
+        self.name = None
+        self.tags = None
+
+    def __init__(self, contestId, index, name, tags):
+        self.contestId = id
+        self.index = index
+        self.name = name
+        self.tags = tags
+
+    def setContestId(self, contestId):
+        self.contestId = contestId
+
+    def setIndex(self, index):
+        self.index = index
+
+    def setNamem(self, name):
+        self.name = name;
+
+    def setTags(self, tags):
+        self.tags = tags
