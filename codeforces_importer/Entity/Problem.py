b586e6c7d1e54de7aa52ba865c753378efb1f9d9
dragonslayer95 <dragonslayerx@outlook.com>
2016-02-17 12:57:14 +0530

    added: SubmissionImporter.getSubmission method

--- codeforces_importer/Entity/Problem.py
+++ codeforces_importer/Entity/Problem.py
@@ -5,11 +5,19 @@ class Problem:
         self.name = None
         self.tags = None
 
-    def __init__(self, contestId, index, name, tags):
-        self.contestId = id
-        self.index = index
-        self.name = name
-        self.tags = tags
+    def __init__(self, probdict):
+        if type(probdict) is __dict__:
+            self.contestId = probdict['id']
+            self.index = probdict['index']
+            self.name = probdict['name']
+            self.tags = probdict['tags']
+
+
+    # def __init__(self, contestId, index, name, tags):
+    #     self.contestId = contestId
+    #     self.index = index
+    #     self.name = name
+    #     self.tags = tags
 
     def setContestId(self, contestId):
         self.contestId = contestId
--- codeforces_importer/SubmissionImporter.py
+++ codeforces_importer/SubmissionImporter.py
@@ -1,13 +1,14 @@
+from codeforces_importer.Entity.Problem import Problem
 from codeforces_importer.urlBuilder import UrlBuilder
 from config import Codeforces
 from config import UserRequestMethod
 from httpRequest import HttpRequest
-
+from Entity.Submission import Submission
 
 class SubmissionImport:
 
     def __init__(self, handle):
-        self.Submission = [];
+        self.submissions = [];
         self.handle = handle;
 
     def generateUrl(self):
@@ -21,3 +22,22 @@ class SubmissionImport:
         url = self.generateUrl()
         response = HttpRequest.sendGetRequest(url);
         return response.json()
+
+    def getSubmissions(self):
+        response = self.importSubmissions();
+
+        if type(response) is __dict__:
+            for subdict in response:
+
+                submission = Submission();
+                if 'id' in subdict:
+                   submission.setId(subdict['id'])
+                if 'contestId' in subdict:
+                    submission.setContestId(subdict['contestId'])
+                if 'problem' in subdict:
+                    submission.setProblem(Problem(subdict['problem']));
+                if 'verdict' in subdict:
+                    submission.setVerdict(subdict['verdict'])
+
+                self.submissions.append(submission);
+
