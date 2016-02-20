e94d2f384bb667846ad8ccd4b80697f12c13352e
dragonslayer <swapnilsaxena@live.in>
2016-02-20 16:05:33 +0530

    released: stable version 1.0

--- main.py
+++ main.py
@@ -1,20 +1,12 @@
 import sys
 import CodeforcesImporter.codeforces_importer.importer
 
-handle = None
-dir_path = None
-
-argv = sys.argv[1:]
-if len(argv) == 2:
-    handle = argv[0]
-    dir_path = argv[1]
-else:
-    handle = str(raw_input("Enter the handle of user: "))
-    dir_path = str(raw_input("Enter the submissions' local dir_path: "))
+handle = str(raw_input("Enter the handle of user: "))
+dir_path = str(raw_input("Enter the submissions' local dir_path: "))
 
 if handle is not None and dir_path is not None:
     print 'Importing submissions of ' + handle + ' at ' + dir_path
     CodeforcesImporter.codeforces_importer.importer.import_codes(handle, dir_path)
 else:
-    print 'Invalid Args'
+    print 'Invalid args'
     sys.exit(1)
