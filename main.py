43311357d472866d620fadfa545eda9bfe5d49ce
ayush awasthi <ayushawasthi04@gmail.com>
2016-02-21 03:50:40 +0530

    added: commandline args

--- main.py
+++ main.py
@@ -1,12 +1,25 @@
 import sys
 import codeforces_importer.importer
+import sys
 
-handle = str(raw_input("Enter the handle of user: "))
-dir_path = str(raw_input("Enter the submissions' local dir_path: "))
+argc = len(sys.argv)
+argv = sys.argv
+
+if argc != 3:
+    print 'Usage ' + argv[0] + " handle local_directory"
+
+    handle = str(raw_input("Enter user's Codeforces handle: "))
+    dir_path = str(raw_input("Enter local_directory_path for import: "))
+
+else :
+    argv = argv[1:]
+
+    handle = argv[0]
+    dir_path = argv[1]
 
 if handle is not None and dir_path is not None:
     print 'Importing submissions of ' + handle + ' at ' + dir_path
     codeforces_importer.importer.import_codes(handle, dir_path)
 else:
     print 'Invalid args'
-    sys.exit(1)
+sys.exit(1)
