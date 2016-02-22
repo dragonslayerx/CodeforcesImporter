b559e4ea77401886898c6f9b2bce330849d82501
dragonslayer <swapnilsaxena@live.in>
2016-02-22 12:50:39 +0530

    added: setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name='CodeforcesImporter',
+    version='v1.0',
+    packages=['', '.idea', 'codeforces_importer', 'codeforces_importer.Entity', 'codeforces_importer.classifier'],
+    url='https://github.com/dragonslayerx/CodeforcesImporter',
+    license='',
+    author='dragonslayerx',
+    author_email='swapnilsaxena@live.in',
+    install_requires=[
+          'lxml', 'jinja2', 'requests'
+    ],
+    description='Just a small script for importing user statistics and past submissions on codeforces'
+)
