932037c8063c8ef6aacda5581d6439552477630f
dragonslayer <swapnilsaxena@live.in>
2016-06-11 15:29:21 +0530

    Fixed: empty package in setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='CodeforcesImporter',
     version='v1.0',
-    packages=['', 'codeforces_importer', 'codeforces_importer.Entity', 'codeforces_importer.classifier'],
+    packages=['codeforces_importer', 'codeforces_importer.Entity', 'codeforces_importer.classifier'],
     url='https://github.com/dragonslayerx/CodeforcesImporter',
     license='',
     author='dragonslayerx',
